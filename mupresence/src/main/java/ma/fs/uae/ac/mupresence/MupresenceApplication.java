99419ae3facd583f74c4239ff0544d1994b553d6
wadiedeSlaoui <titatita199681@gmail.com>
2025-06-02 15:06:50 +0100

    first commit

---
+++ mupresence/src/main/java/ma/fs/uae/ac/mupresence/MupresenceApplication.java
@@ -0,0 +1,13 @@
+package ma.fs.uae.ac.mupresence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MupresenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MupresenceApplication.class, args);
+	}
+
+}
---
+++ mupresence/src/test/java/ma/fs/uae/ac/mupresence/MupresenceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fs.uae.ac.mupresence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MupresenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
