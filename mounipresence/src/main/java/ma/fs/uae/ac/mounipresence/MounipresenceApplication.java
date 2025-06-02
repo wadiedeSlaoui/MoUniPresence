92337b49203e2c1b7603cb555ad074f7cd9b9d85
wadiedeSlaoui <titatita199681@gmail.com>
2025-06-02 14:57:45 +0100

    first commit

---
+++ mounipresence/src/main/java/ma/fs/uae/ac/mounipresence/MounipresenceApplication.java
@@ -0,0 +1,13 @@
+package ma.fs.uae.ac.mounipresence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MounipresenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MounipresenceApplication.class, args);
+	}
+
+}
---
+++ mounipresence/src/test/java/ma/fs/uae/ac/mounipresence/MounipresenceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fs.uae.ac.mounipresence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MounipresenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
