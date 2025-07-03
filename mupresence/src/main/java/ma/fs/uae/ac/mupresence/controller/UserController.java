eb2e9e01f50ac94b69d5b818eb5f496335c5b30f
wadiedeSlaoui <titatita199681@gmail.com>
2025-07-03 16:50:35 +0100

    Add List of User

--- mupresence/src/main/java/ma/fs/uae/ac/mupresence/controller/UserController.java
+++ mupresence/src/main/java/ma/fs/uae/ac/mupresence/controller/UserController.java
@@ -1,20 +1,23 @@
 package ma.fs.uae.ac.mupresence.controller;
 
+import ma.fs.uae.ac.mupresence.dto.UserDTO;
+import ma.fs.uae.ac.mupresence.model.UserEntity;
+import ma.fs.uae.ac.mupresence.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
-
-    @GetMapping("/test")
-    public String test(){
-        return "test";
-    }
-    @GetMapping("/testWithoutCred")
-    public String testWithoutCred(){
-        return "testWithoutCred";
+    @Autowired
+    private UserService userService;
+    @GetMapping("")
+    public List<UserDTO> getAllUsers() {
+        return userService.getUsers();
     }
 
 }
--- mupresence/src/main/java/ma/fs/uae/ac/mupresence/services/UserService.java
+++ mupresence/src/main/java/ma/fs/uae/ac/mupresence/services/UserService.java
@@ -4,10 +4,14 @@ package ma.fs.uae.ac.mupresence.services;
 import ma.fs.uae.ac.mupresence.dto.AuthRequest;
 import ma.fs.uae.ac.mupresence.dto.UserDTO;
 
+import java.util.List;
+
 public interface UserService {
 
     void registerUser(UserDTO userDTO);
     String createAuthenticationToken (AuthRequest authRequest);
 
     UserDTO getUser(String username);
+
+    List<UserDTO> getUsers();
 }
